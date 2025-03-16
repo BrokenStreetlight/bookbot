8b7f34cfff00f4fed76b558472235e476213add0
BrokenStreetlight <adam.clark1213@gmail.com>
2025-03-16 11:08:03 -0400

    added Enum and textnode

---
+++ src/textnode.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    NORMAL = "normal"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, obj):
+        if self.text != obj.text:
+            return False
+        if self.text_type != obj.text_type:
+            return False
+        if self.url != obj.url:
+            return False
+        return True
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
