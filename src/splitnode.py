78f8256855ffdabd965930ea6f0b10fa4ddf5ee0
BrokenStreetlight <adam.clark1213@gmail.com>
2025-03-22 10:40:31 -0400

    wip the split_nodes_images function

--- src/splitnode.py
+++ src/splitnode.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from extractmarkdown import extract_markdown_images, extract_markdown_links
 
 
 def split_nodes_delimiter(
@@ -20,3 +21,26 @@ def split_nodes_delimiter(
                 new_nodes.append(TextNode(text, text_type))
                 flip = True
     return new_nodes
+
+
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    for old_node in old_nodes:
+        images = extract_markdown_images(old_node.text)
+        old_text = old_node.text
+        for image in images:
+            image_alt = image[0]
+            image_link = image[1]
+            section = old_text.split(f"![{image_alt}]({image_link})", 1)
+            print(f"section:{section}")
+            print(f"old_text:{old_text}")
+            old_text = old_text.replace(section[0])
+            print(f"post_old_text:{old_text}")
+            print("----")
+
+
+if __name__ == "__main__":
+    node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT,
+    )
+    new_nodes = split_nodes_image([node])
