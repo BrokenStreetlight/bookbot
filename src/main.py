177b8f08e1d0bbd6546ead63a4bd5e9c0e45269d
BrokenStreetlight <adam.clark1213@gmail.com>
2025-03-23 11:04:27 -0400

    Added logger

--- src/main.py
+++ src/main.py
@@ -1,10 +1,32 @@
+import logging
+
 from textnode import TextNode, TextType
+
+
+def setup_file_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    # Create a file handler that logs even debug messages
+    fh = logging.FileHandler("bookbot.log")
+    fh.setLevel(logging.DEBUG)
+
+    # Create formatter
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    fh.setFormatter(formatter)
+
+    # Add the handler to the logger
+    logger.addHandler(fh)
+    return logger
 
 
 def main():
     t1 = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(t1)
+    logger.debug("TextNode: %s", t1)
 
 
 if __name__ == "__main__":
+    logger = setup_file_logger()
     main()
